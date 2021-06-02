5d478b8ca5e07175cb00a91e8f68ad920e64a059
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2021-06-02 10:19:02 +0300

    Add files via upload

---
+++ CVectRat.cpp
@@ -0,0 +1,69 @@
+#include "Func.h"
+#include "CRat.h"
+#include "CVectRat.h"
+CVectRat& CVectRat :: operator=(const CVectRat& b) {
+	for (int i = 0; i < size_; i++) {
+		arr[i] = b.arr[i];
+	}
+	size_ = b.size();
+
+	return *this;
+};
+
+gorCVectRat operator+(const CVectRat& a, const CVectRat& b) {
+	gorCVectRat m(a.size());
+	for (int i = 0; i < m.size(); i++) {
+		m.arr[i] = a.get(i) + b.get(i);
+	}
+	return m;
+};
+
+//gorCVectRat operator-(const CVectRat& a, const CVectRat& b) {
+	//gorCVectRat n(a.size());
+	//for (int i = 0; i < n.size(); i++) {
+		//n.arr[i] = a.get(i) - b.get(i);
+	//}
+	//return n;
+//};
+
+CRat operator*(const CVectRat& a, const CVectRat& b) {
+	CRat m;
+	for (int i = 0; i < a.size(); i++) {
+		m = m + a.get(i)*b.get(i);
+	}
+	return m;
+};
+int CVectRat::size() const {
+	return size_;
+}
+
+
+//void CVectRat::print() const {
+	//cout << "(";
+	//for (int i = 0; i < size_ - 1; i++) {
+	//	arr[i].print();
+	//	cout << "\n";
+	//}
+	//arr[size_ - 1].print();
+	//cout << ")\n";
+//}
+
+void vertCVectRat::print() const {
+	cout << "(";
+	for (int i = 0; i < size_ - 1; i++) {
+		arr[i].print();
+		cout << "\n";
+	}
+	arr[size_ - 1].print();
+	cout << ")\n";
+}
+
+void gorCVectRat::print() const {
+	cout << "(";
+	for (int i = 0; i < size_ - 1; i++) {
+		arr[i].print();
+		cout << " , ";
+	}
+	arr[size_ - 1].print();
+	cout << ")\n";
+}
---
+++ input.cpp
@@ -0,0 +1,33 @@
+#include "Func.h"
+#include "CRat.h"
+#include "CVectRat.h"
+
+CVectRat input(int size) {
+	
+	int vecType = -1;
+	std::cout << "Input 0 for horizontal vector and 1 for vertical:\n";
+	std::cin >> vecType;
+	if (vecType !=0 && vecType != 1) {
+		std::cerr << "Error! Wrong input!\n";
+		exit(1);
+	}
+
+	CVectRat* m = (vecType == 0) ? dynamic_cast<CVectRat*>(new gorCVectRat(size)) : dynamic_cast<CVectRat*>(new vertCVectRat(size));
+	unsigned den;
+	unsigned num;
+	//int num;
+	for (int i = 0; i < size; i++) {
+		cout << "Vvedite " << i + 1 << "-yu koordinatu (chisl i znamen)\t";
+		cin >> num;
+		cin >> den;
+		CRat a(num, den);
+		m->Set_arr(i, a);
+	}
+	cout << "Vy vveli vector\t\n\n";
+	m->print();
+	cout << endl;
+
+	//delete m;
+
+	return *m;
+};
